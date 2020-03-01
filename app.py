65f727f40beb65fc6de86ebcd26cc75df5fab4be
EC2 Default User <ec2-user@ip-172-31-34-216.ec2.internal>
2020-03-01 17:32:50 +0000

    update app

--- app.py
+++ app.py
@@ -4,16 +4,18 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 
 def train_iris(train_fraction=0.8):
-    data = load_iris()
     
+    print("Loading Iris data...")
+    data = load_iris()
     X = data.data
     Y = data.target
-    
     X_train, X_test, Y_train, Y_test = train_test_split(
         X, Y, test_size=1.-train_fraction)
 
+    print("Training a logistic regression model...")
     lr = LogisticRegression(C=1e5, solver='liblinear')
     lr.fit(X_train, Y_train)
+    print("Model successfully trained...")
 
     print("Testing Accuracy:",
             lr.score(X_test, Y_test))
